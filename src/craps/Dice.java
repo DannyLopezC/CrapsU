01b1776d6f4c461e01221326b17829ab7896d9a0
Danny López <63324885+Hyud1@users.noreply.github.com>
2021-02-19 14:17:54 -0500

    Setting up dice class

--- src/craps/Dice.java
+++ src/craps/Dice.java
@@ -1,5 +1,17 @@
 package craps;
 
-public class Dice {
+import java.util.Random;
 
+public class Dice {
+	private int visibleFace;
+	
+	private void setVisibleFace() {
+		Random random = new Random();
+		visibleFace = random.nextInt(6) + 1;
+	}
+	
+	public int getVisibleFace() {
+		setVisibleFace();
+		return visibleFace;
+	}
 }
